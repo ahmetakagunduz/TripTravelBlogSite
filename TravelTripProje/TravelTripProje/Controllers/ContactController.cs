0c43be43b11e99a1a0eb747271b29cc6c0e32027
Ahmetxyz <ahmet.akagundz@gmail.com>
2024-05-02 01:46:30 +0300

    Trip&Travel blog site project with asp.net mvc5

---
+++ TravelTripProje/TravelTripProje/Controllers/ContactController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TravelTripProje.Models.Classes;
+
+namespace TravelTripProje.Controllers
+{
+    public class ContactController : Controller
+    {
+        // GET: Contact
+        Context c = new Context();
+        public ActionResult Index()
+        {
+            var degerler = c.iletisims.ToList();
+            return View(degerler);
+        }
+    }
+}
---
+++ TravelTripProje/TravelTripProje/Models/Classes/BlogYorum.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TravelTripProje.Models.Classes;
+
+namespace TravelTripProje.Models.Classes
+{
+    public class BlogYorum
+    {
+        public IEnumerable<Blog> Deger1 { get; set; }
+        public IEnumerable<Yorumlar> Deger2 { get; set; }
+        public IEnumerable<Blog> Deger3 { get; set; }
+
+        // birden fazla tablodan veri cekmek istedigimizde IEnumerable kullanilir
+    }
+}
